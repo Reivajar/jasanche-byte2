3783385a8f4157a756ebe73bd57ddc4fe1cf4740
Javier Argota <reivajar@gmail.com>
2017-02-05 18:39:50 -0500

    corrections

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 # this is used for constructing URLs to google's APIS
 from googleapiclient.discovery import build
 
-API_KEY = AIzaSyDEvexqbZDpzFw2Inmoa1sRAWIyIqYapVM
+API_KEY = 'AIzaSyDEvexqbZDpzFw2Inmoa1sRAWIyIqYapVM'
 
 # This uses discovery to create an object that can talk to the 
 # fusion tables API using the developer key
