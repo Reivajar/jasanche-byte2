dd85c0b295325eb048101de8c8840d8f1480054d
Javier Argota <reivajar@gmail.com>
2017-02-05 17:22:56 -0500

    main.py updated

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+# Imports
+import os
+import jinja2
+import webapp2
+import logging
+import json
+import urllib
+
+# this is used for constructing URLs to google's APIS
+from googleapiclient.discovery import build
+
+# This uses discovery to create an object that can talk to the 
+# fusion tables API using the developer key
+service = build('fusiontables', 'v1', developerKey=API_KEY)
+
+TABLE_ID = '17xpBND6OU2obf5e1zUfkdzjTvx_OYIm8lS7FHJ81'
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
